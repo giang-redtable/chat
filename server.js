a88e682e294bbb229ff27a3cf89383767bf436b7
gliderifle <gliderifle@gmail.com>
2017-12-07 17:49:12 +0900

    giang first

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ io.on('connection', function(socket){
     io.sockets.in('channel'+ data.channelId).emit('receive message', msg);   
 
     if(data.initSet==''){
-      if(!(data.msg==1 || data.msg==2)){
+      if(!(data.msg==1 || data.msg==2 || data.msg==3 || data.msg==4)){
         msgRe = '1 (한식) 또는 2 (中餐)를 입력해 주세요.';
         apiResponse(msgRe);
       } else {
@@ -68,43 +68,82 @@ io.on('connection', function(socket){
                 apiResponse('다음 메뉴 중 좋아하시는 메뉴를 선택하시면, 추천해드릴게요');                
                 setTimeout(()=>firstQuestion(data.msg),1000);
             }
-        })       
+        })
       }
     } else {
-      lang = data.initSet == "1" ? "kr":"cn";
-      var url = "https://api.redtable.global/chatbot/recommend.php?user="+socket.id+"&method=sample&menu=kr06,kr05&index=0";
-      var retValue = '이런 메뉴는 어떠신가요? <br/>';
+	  lang = "kr";
+	  if (data.initSet == "2") { lang = "cn" }
+	  else if (data.initSet == "3") { lang = "jp" }
+	  else if (data.initSet == "4") { lang = "en" }
+      
+	  var retValue = '이런 메뉴는 어떠신가요? (y/n)<br/>';
+	  var msg = data.msg;
+	  var preData = JSON.parse(data.preData);
       //console.log('baseCd: '+data.recomIdx);
       //console.log('recomIdx: '+data.recomIdx);
 
       if(data.recomIdx==0) {
         //첫 추천은 API call 50개의 추천 메뉴정보 가져옴
+		var idList = msg.split(",");
+		var ids = "";
+		for (var i = 0; i < idList.length; i++) {
+			ids += "," + preData[idList[i]].code;
+		}
+		ids = ids.substring(1);
+		io.sockets.in('channel'+ data.channelId).emit('base cd', ids);
+		
+		var url = "https://api.redtable.global/chatbot/recommend.php?user="+socket.id+"&method=sample&menu="+ids+"&index=0";
+		
         var req = request({
             url: url,
             json: true
         }, (error, response, body) => {
-            io.sockets.in('channel'+ data.channelId).emit('pre data', body);  
+			io.sockets.in('channel'+ data.channelId).emit('pre data', body.result);  
             //console.log(body);
-            if (!error && response.statusCode === 200) {
-                for(var i in body) {
-                  for(var j=data.recomIdx;j < Number(data.recomIdx)+3; j++) {                    
-                    retValue += body[i][j].name + '<br>';
-                  }
-                }
-                io.sockets.in('channel'+ data.channelId).emit('recom idx', Number(data.recomIdx)+3);  
+            if (!error && response.statusCode === 200) {            
+				retValue += body.result[0].name + '<br>';
+                io.sockets.in('channel'+ data.channelId).emit('recom idx', 1);
                 apiResponse(retValue);
             }
-        }) 
+        })
       } else {
         //console.log(data.preData);
-        var body = JSON.parse(data.preData);
-        for(var i in body) {
-          for(var j=data.recomIdx;j < Number(data.recomIdx)+3; j++) {                    
-            retValue += body[i][j].name + '<br>';
-          }
-        }
-        io.sockets.in('channel'+ data.channelId).emit('recom idx', Number(data.recomIdx)+3);  
-        apiResponse(retValue);
+		
+		if (msg == "y") {
+			//메뉴 선택
+			var url = "api.redtable.global/chatbot/recommend.php?user="+socket.id+"&method="+data.method+"&menu="+data.baseCd+"&index=0";
+		} else if (data.recomIdx % 50 == 0) {
+			//다음 메뉴
+			var url = "https://api.redtable.global/chatbot/recommend.php?user="+socket.id+"&method=sample&menu="+data.baseCd+"&index=" + data.recomIdx;
+			
+			var req = request({
+				url: url,
+				json: true
+			}, (error, response, body) => {
+				!error && response.statusCode === 200) {            
+					io.sockets.in('channel'+ data.channelId).emit('pre data', body.result);  
+					retValue += body.result[0].name + '<br>';
+					io.sockets.in('channel'+ data.channelId).emit('recom idx', data.recomIdx + 1);
+					apiResponse(retValue);
+				}
+			})
+			
+			var req = request({
+				url: url,
+				json: true
+			}, (error, response, body) => {
+				!error && response.statusCode === 200) {            
+					io.sockets.in('channel'+ data.channelId).emit('pre data', body.result);  
+					retValue += body.result[0].name + '<br>';
+					io.sockets.in('channel'+ data.channelId).emit('recom idx', data.recomIdx + 1);
+					apiResponse(retValue);
+				}
+			})
+		} else {
+			retValue += preData[data.recomIdx % 50].name + '<br>';
+            io.sockets.in('channel'+ data.channelId).emit('recom idx', data.recomIdx + 1);
+            apiResponse(retValue);
+		}
       }
     }
 
@@ -112,7 +151,11 @@ io.on('connection', function(socket){
       var lang;
       var retValue = "";
 
-      lang = var1 == 1 ? "kr":"cn";
+	  lang = "kr";
+	  if (var1 == 2) { lang = "cn" }
+	  else if (var1 == 3) { lang = "jp" }
+	  else if (var1 == 4) { lang = "en" }
+      
       var url = "https://api.redtable.global/chatbot/sample.php?lang="+lang;
       
       var req = request({
@@ -127,10 +170,34 @@ io.on('connection', function(socket){
                   retValue += j +'. ' + body[i][j].name + '<br>';
                 }
               }
+			  io.sockets.in('channel'+ data.channelId).emit('pre data', body.result);  
+			  io.sockets.in('channel'+ data.channelId).emit('method', 'sample');  
               apiResponse(retValue);
           }
       })     
-    }    
+    }
+	
+	var historyQuestion = function(var1,callback) {
+      var lang;
+      var retValue = "";
+
+	  var url = "https://api.redtable.global/chatbot/recommend.php?user="+socket.id+"&method=history&index=0";
+      
+      var req = request({
+			url: url,
+			json: true
+		}, (error, response, body) => {
+			if (!error && response.statusCode === 200) {
+				retValue = '이런 메뉴는 어떠신가요? (y/n)<br/>' + body.result[0].name + '<br>';
+				io.sockets.in('channel'+ data.channelId).emit('pre data', body.result);  
+				io.sockets.in('channel'+ data.channelId).emit('recom idx', 0);
+				io.sockets.in('channel'+ data.channelId).emit('method', 'history');
+				io.sockets.in('channel'+ data.channelId).emit('base cd', '');
+				
+				apiResponse(retValue);
+			}
+		})    
+    }
     
     function apiResponse(retMsg){   
 	  msgRT = '<div class="comment"><a class="avatar"><img src="/imgs/avatar/5.jpg"></a><div class="content"><a class="author">REDTABLE</a><div class="metadata">';
