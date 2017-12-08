ccd8d9f8e2644c8f0cc897d946ca1504e2b940ee
gliderifle <gliderifle@gmail.com>
2017-12-08 16:54:42 +0900

    restart or continue after 50 recommend menus

--- server.js
+++ server.js
@@ -35,143 +35,150 @@ io.on('connection', function(socket){
   });
 
   socket.on('send message', function(data){
+    var msg;
+    var msgRT;
+    var msgRe;
+    var avatarNum = (data.userName % 4) + 1;
 
-  var msg;
-  var msgRT;
-  var msgRe;
-	var avatarNum = (data.userName % 4) + 1;
-
-	msg = '<div class="comment" style="text-align:right"><a class="avatar" style="float:right;"><img src="/imgs/avatar/'+ avatarNum +'.jpg"></a><div class="content" style="margin-left:0;margin-right:3.5em;"><a class="author">Guest('+ socket.id +')</a><div class="metadata">';
-	msg += '<span class="date">'+ getTimeStamp() +'</span></div>'
-	msg += '<div class="text">'+ data.msg + '</div></div></div>';
+    msg = '<div class="comment" style="text-align:right"><a class="avatar" style="float:right;"><img src="/imgs/avatar/'+ avatarNum +'.jpg"></a><div class="content" style="margin-left:0;margin-right:3.5em;"><a class="author">Guest('+ socket.id +')</a><div class="metadata">';
+    msg += '<span class="date">'+ getTimeStamp() +'</span></div>'
+    msg += '<div class="text">'+ data.msg + '</div></div></div>';
     
     io.sockets.in('channel'+ data.channelId).emit('receive message', msg);   
 
-    if(data.initSet==''){
-      if(!(data.msg==1 || data.msg==2 || data.msg==3 || data.msg==4)){
+    if(data.initSet=='') {
+      //Choose language
+      if(!(data.msg==1 || data.msg==2 || data.msg==3 || data.msg==4)) {
         msgRe = '1 (한식) 또는 2 (中餐)를 입력해 주세요.';
         apiResponse(msgRe);
       } else {
         io.sockets.in('channel'+ data.channelId).emit('init set', data.msg); 
-               
+                
         //login API
 
         var url = "https://api.redtable.global/chatbot/userreg.php?user="+socket.id;        
         var req = request({
-            url: url,
-            json: true
+          url: url,
+          json: true
         }, (error, response, body) => {
-        
-            if (!error && response.statusCode === 200) {
-                apiResponse('지금부터 몇개의 메뉴를 보여드릴거에요 마음에 드시면 "Y" 아니시면 "N"을 입력해 주세요.');                
-                setTimeout(()=>firstQuestion(data.msg),1000);
-            }
+          if (!error && response.statusCode === 200) {
+            apiResponse('지금부터 몇개의 메뉴를 보여드릴거에요 마음에 드시면 "Y" 아니시면 "N"을 입력해 주세요.');                
+            setTimeout(()=>firstQuestion(data.msg),1000);
+          }
         })
       }
     } else {
+        var sampleData = JSON.parse(data.sampleData);
+        var sampleSelect = JSON.parse(data.sampleSelect);
+        
+        lang = "kr";
+        if (data.initSet == "2") { lang = "cn" }
+        else if (data.initSet == "3") { lang = "jp" }
+        else if (data.initSet == "4") { lang = "en" }
+        
+        if(sampleData.length > 0) { //Sample data 남아있을 경우 계속 보여주면서  N일 경우 해당code pop
 
-    var sampleData = JSON.parse(data.sampleData);      
-    var sampleSelect = JSON.parse(data.sampleSelect);  
-    
-    lang = "kr";
-	  if (data.initSet == "2") { lang = "cn" }
-	  else if (data.initSet == "3") { lang = "jp" }
-    else if (data.initSet == "4") { lang = "en" }
-    
-    if(sampleData.length > 0) { //Sample data 남아있을 경우 계속 보여주면서  N일 경우 해당code pop
+          if(data.msg==="n" || data.msg==="N" || data.msg==="no" || data.msg==="No") {
+            sampleSelect.pop();
+          }
+          
+          //Sample data view
+          SampleQuestion(sampleData, sampleSelect);
 
-      if(data.msg==="n" || data.msg==="N" || data.msg==="no" || data.msg==="No") {
-        sampleSelect.pop();
-      }
-      
-      //Sample data view
-      SampleQuestion(sampleData, sampleSelect);
+        } else {
+          if (data.preData==='' && data.recomIdx==0){
+            if(data.msg==="n" || data.msg==="N" || data.msg==="no" || data.msg==="No") {
+              sampleSelect.pop();
+            }
+          } else {
+            var preData = JSON.parse(data.preData);
+          }
 
-    } else {
+          var retValue = '<h5>이런 메뉴는 어떠신가요? (Y/N)</h5>';
+          var msg = data.msg;
+          //var preData = JSON.parse(data.preData);      
 
-      if(data.preData==='' && data.recomIdx==0){
-        if(data.msg==="n" || data.msg==="N" || data.msg==="no" || data.msg==="No") {
-          sampleSelect.pop();          
-        }
-      } else {
-        var preData = JSON.parse(data.preData);      
-      }
+          if(data.recomIdx==0) {
+          //첫 추천은 API call 50개의 추천 메뉴정보 가져옴
+          
+            var ids = "";
 
-      var retValue = '<h5>이런 메뉴는 어떠신가요? (Y/N)</h5>';
-      var msg = data.msg;
-      //var preData = JSON.parse(data.preData);      
-
-      if(data.recomIdx==0) {
-      //첫 추천은 API call 50개의 추천 메뉴정보 가져옴
-      
-      var ids = "";
-
-      for (var i = 0; i < sampleSelect.length; i++) {
-        ids += "," + sampleSelect[i];
-      }
-      
-      ids = ids.substring(1);
-      
-      io.sockets.in('channel'+ data.channelId).emit('base cd', ids);
-      
-      var url = "https://api.redtable.global/chatbot/recommend.php?user="+socket.id+"&method=sample&menu="+ids+"&index=0";
-      
-          var req = request({
-              url: url,
-              json: true
-          }, (error, response, body) => {
-          io.sockets.in('channel'+ data.channelId).emit('pre data', body.result);
-          io.sockets.in('channel'+ data.channelId).emit('sample select', sampleSelect);
-              
-          //console.log(body);
-              if (!error && response.statusCode === 200) {            
+            for (var i = 0; i < sampleSelect.length; i++) {
+              ids += "," + sampleSelect[i];
+            }
+            
+            ids = ids.substring(1);
+            
+            io.sockets.in('channel'+ data.channelId).emit('base cd', ids);
+          
+            var url = "https://api.redtable.global/chatbot/recommend.php?user="+socket.id+"&method=sample&menu="+ids+"&index=0";
+          
+            var req = request({
+                url: url,
+                json: true
+            }, (error, response, body) => {
+              io.sockets.in('channel'+ data.channelId).emit('pre data', body.result);
+              io.sockets.in('channel'+ data.channelId).emit('sample select', sampleSelect);
+                  
+              //console.log(body);
+              if (!error && response.statusCode === 200) {
                   retValue += body.result[0].name + '<br>';
                   io.sockets.in('channel'+ data.channelId).emit('recom idx', 1);
                   apiResponse(retValue);
               }
-          })
-        } else {        
-      
-      if (msg == "y") {
-        //메뉴 선택
-        var url = "https://api.redtable.global/v3/restaurant_partner.php?city=seoul&search=초밥";
-        retValue = '<h5>선택하신 메뉴를 판매하는 레스토랑 정보를 드릴까요?</h5>';
-        retValue += '<a class="ui button blue">Yes</a> or <a class="ui button red">No</a>?';     
-        apiResponse(retValue);
+            })
+          } else {
+            console.log(msg);
+            if (msg == "y") {
+              if (preData.length == 0) {
+                io.sockets.in('channel'+ data.channelId).emit('recom idx', '');
+                io.sockets.in('channel'+ data.channelId).emit('init set', '');
+                io.sockets.in('channel'+ data.channelId).emit('base cd', '');
+                io.sockets.in('channel'+ data.channelId).emit('sample select', '');
 
-      } else if (data.recomIdx == 0) {
-        //다음 메뉴
-        var url = "https://api.redtable.global/chatbot/recommend.php?user="+socket.id+"&method=sample&menu="+data.baseCd+"&index=" + data.recomIdx;
-        
-        var req = request({
-          url: url,
-          json: true
-        }, (error, response, body) => {
-          if(!error && response.statusCode === 200) {
+                msgRe = '1 (한식) 또는 2 (中餐)를 입력해 주세요.';
+                apiResponse(msgRe);
+              } else {
+                //메뉴 선택
+                var url = "https://api.redtable.global/v3/restaurant_partner.php?city=seoul&search=" + preData[0].name;
+                retValue = '<h5>선택하신 메뉴를 판매하는 레스토랑 정보를 드릴까요?</h5>';
+                retValue += '<a class="ui button blue">Yes</a> or <a class="ui button red">No</a>?';     
+                apiResponse(retValue);
+              }
+            } else if (data.recomIdx % 50 == 0) {
+              if (preData.length > 0) {
+                retValue = '<h5>추천 메뉴가 마음에 안드시면 처음부터 다시 진행하시겠습니까?</h5>';
+                retValue += '<a class="ui button blue">Yes</a> or <a class="ui button red">No</a>?';
+                io.sockets.in('channel'+ data.channelId).emit('pre data', '');
+                apiResponse(retValue);
+              } else {
+                //다음 메뉴
+                var url = "https://api.redtable.global/chatbot/recommend.php?user="+socket.id+"&method=sample&menu="+data.baseCd+"&index=" + data.recomIdx;
+                
+                var req = request({
+                  url: url,
+                  json: true
+                }, (error, response, body) => {
+                  if (!error && response.statusCode === 200) {
+                    retValue += '<strong>' + body.result[0].name + '</strong><br/>';
 
-            retValue += '<strong>' + body.result[0].name + '</strong><br/>';
-
-            io.sockets.in('channel'+ data.channelId).emit('pre data', body.result);              
-            io.sockets.in('channel'+ data.channelId).emit('recom idx', data.recomIdx + 1);
-            apiResponse(retValue);
-          }
-        })        
-        
-      } else {
-            if(data.recomIdx % 50==0){
-              retValue = '<h5>추천 메뉴가 마음에 안드시면 처음부터 다시 진행하시겠습니까?</h5>';
-              retValue += '<a class="ui button blue">Yes</a> or <a class="ui button red">No</a>?';           
+                    io.sockets.in('channel'+ data.channelId).emit('pre data', body.result);
+                    io.sockets.in('channel'+ data.channelId).emit('recom idx', Number(data.recomIdx) + 1);
+                    apiResponse(retValue);
+                  }
+                })
+              }
             } else {
-              var arrData = preData.shift();
-              //retValue += preData[data.recomIdx % 50].name + '<br>';
+              var arrData = preData[1];
+              preData.shift();
               retValue += arrData.name + '<br>';
+
+              io.sockets.in('channel'+ data.channelId).emit('pre data', preData);
+              io.sockets.in('channel'+ data.channelId).emit('recom idx', Number(data.recomIdx) + 1);
+              apiResponse(retValue);
             }
-            io.sockets.in('channel'+ data.channelId).emit('pre data', preData);
-            io.sockets.in('channel'+ data.channelId).emit('recom idx', data.recomIdx + 1);
-            apiResponse(retValue);
-      }
+          }
         }
-      }
     }
 
     var firstQuestion = function(var1,callback) {
@@ -191,65 +198,64 @@ io.on('connection', function(socket){
           url: url,
           json: true
       }, (error, response, body) => {
-      
-          if (!error && response.statusCode === 200) {
-            
-            arrData = body.result.shift();
-            sampleSelect = [arrData.code];
+        if (!error && response.statusCode === 200) {
+          
+          arrData = body.result.shift();
+          sampleSelect = [arrData.code];
 
-            retValue = '<img src="/imgs/'+ arrData.code +'.png" />';
-            retValue += '<h3>'+ arrData.name + '</h3> <a class="ui button blue">Yes</a> or <a class="ui button red">No</a>?';            
-            
-            io.sockets.in('channel'+ data.channelId).emit('sample data', body.result);
-            io.sockets.in('channel'+ data.channelId).emit('sample select', sampleSelect);
-			      io.sockets.in('channel'+ data.channelId).emit('method', 'sample');  
-            
-            apiResponse(retValue);
-          }
+          retValue = '<img src="/imgs/'+ arrData.code +'.png" />';
+          retValue += '<h3>'+ arrData.name + '</h3> <a class="ui button blue">Yes</a> or <a class="ui button red">No</a>?';            
+          
+          io.sockets.in('channel'+ data.channelId).emit('sample data', body.result);
+          io.sockets.in('channel'+ data.channelId).emit('sample select', sampleSelect);
+          io.sockets.in('channel'+ data.channelId).emit('method', 'sample');  
+          
+          apiResponse(retValue);
+        }
       })     
     }
   
-  function SampleQuestion(var1, var2) {    
+    function SampleQuestion(var1, var2) {
     
-    var retValue = "";    
-    var arrData = var1.shift();    
-    var2.push(arrData.code);        
+      var retValue = "";    
+      var arrData = var1.shift();    
+      var2.push(arrData.code);        
 
-    retValue = '<img src="/imgs/'+ arrData.code +'.png"/>';
-    retValue += '<h3>'+ arrData.name + '</h3> <a class="ui button blue">Yes</a> or <a class="ui button red">No</a>?';     
+      retValue = '<img src="/imgs/'+ arrData.code +'.png"/>';
+      retValue += '<h3>'+ arrData.name + '</h3> <a class="ui button blue">Yes</a> or <a class="ui button red">No</a>?';     
 
-    io.sockets.in('channel'+ data.channelId).emit('sample data', var1);
-    io.sockets.in('channel'+ data.channelId).emit('sample select', var2);
-    
-    apiResponse(retValue);
-  }
+      io.sockets.in('channel'+ data.channelId).emit('sample data', var1);
+      io.sockets.in('channel'+ data.channelId).emit('sample select', var2);
+      
+      apiResponse(retValue);
+    }
 	
-	var historyQuestion = function(var1,callback) {
+	  var historyQuestion = function(var1,callback) {
       var lang;
       var retValue = "";
 
-	  var url = "https://api.redtable.global/chatbot/recommend.php?user="+socket.id+"&method=history&index=0";
-      
-      var req = request({
-			url: url,
-			json: true
-		}, (error, response, body) => {
-			if (!error && response.statusCode === 200) {
-				retValue = '이런 메뉴는 어떠신가요? (y/n)<br/>' + body.result[0].name + '<br>';
-				io.sockets.in('channel'+ data.channelId).emit('pre data', body.result);  
-				io.sockets.in('channel'+ data.channelId).emit('recom idx', 0);
-				io.sockets.in('channel'+ data.channelId).emit('method', 'history');
-				io.sockets.in('channel'+ data.channelId).emit('base cd', '');
-				
-				apiResponse(retValue);
-			}
-		})    
+      var url = "https://api.redtable.global/chatbot/recommend.php?user="+socket.id+"&method=history&index=0";
+        
+        var req = request({
+        url: url,
+        json: true
+      }, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          retValue = '이런 메뉴는 어떠신가요? (y/n)<br/>' + body.result[0].name + '<br>';
+          io.sockets.in('channel'+ data.channelId).emit('pre data', body.result);  
+          io.sockets.in('channel'+ data.channelId).emit('recom idx', 0);
+          io.sockets.in('channel'+ data.channelId).emit('method', 'history');
+          io.sockets.in('channel'+ data.channelId).emit('base cd', '');
+          
+          apiResponse(retValue);
+        }
+      })    
     }
     
-    function apiResponse(retMsg){   
-	  msgRT = '<div class="comment"><a class="avatar"><img src="/imgs/avatar/5.jpg"></a><div class="content"><a class="author">REDTABLE</a><div class="metadata">';
-	  msgRT += '<span class="date">'+ getTimeStamp() +'</span></div>'
-	  msgRT += '<div class="text">'+ retMsg +'</div></div></div>';
+    function apiResponse(retMsg) {
+	    msgRT = '<div class="comment"><a class="avatar"><img src="/imgs/avatar/5.jpg"></a><div class="content"><a class="author">REDTABLE</a><div class="metadata">';
+	    msgRT += '<span class="date">'+ getTimeStamp() +'</span></div>'
+	    msgRT += '<div class="text">'+ retMsg +'</div></div></div>';
       io.sockets.in('channel'+ data.channelId).emit('receive message', msgRT);
     }
 
